6a1464182904a1d8574b3720b1e6da51f6f74132
bernddorn <bernddorn@d979326c-c14f-11dd-ad05-db3d840e29cc>
2008-12-15 14:19:18 +0000

    see changes, version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lovely.jsonrpc",
-    version="0.0.1a1",
+    version="0.0.1a2",
     packages=find_packages('src'),
     package_dir = {'':'src'},
     zip_safe = True,
