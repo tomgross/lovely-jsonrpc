f94d74b86ffaae15313cd022d996eed040dc829e
bernd.roessl <bernd.roessl@d979326c-c14f-11dd-ad05-db3d840e29cc>
2009-06-09 10:15:59 +0000

    pimped setup.py to fit for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 import os
 from setuptools import setup, find_packages
 
+longDesc = """
+
+This package includes json-rpc client and server (wsgi) implementation.
+
+"""
+
 setup(
     name="lovely.jsonrpc",
     version="0.0.1a5",
+    license = "Apache License 2.0",
+    description = 'Python JSONRPC package',
+    long_description = longDesc,
+    keywords = "JSON RPC JSONRPC WSGI",
+    author = "Lovely Systems AG",
+    author_email = "bernddorn@gmail.com",
+    url = 'http://code.google.com/p/lovely-jsonrpc',
     packages=find_packages('src'),
     package_dir = {'':'src'},
     zip_safe = True,
     include_package_data = False,
+    platforms = ('Any',),
     namespace_packages = ['lovely'],
     install_requires = ['setuptools'],
-    extras_require = dict(
-                          test=['zope.testing', 'simplejson'],
-                          ),
+    extras_require = dict(test=['zope.testing', 'simplejson']),
     )
