6f1177a05615bb634e675e3e32295006a7755c50
bernddorn@gmail.com <bernddorn@gmail.com@d979326c-c14f-11dd-ad05-db3d840e29cc>
2010-10-22 06:13:01 +0000

    Tagged lovely.jsonrpc 0.2.1.

---
+++ tags/0.2.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+longDesc = """
+
+This package includes json-rpc client and server (wsgi) implementation.
+
+"""
+
+setup(
+    name="lovely.jsonrpc",
+    version="0.2.1",
+    license = "Apache License 2.0",
+    description = 'Python JSONRPC package',
+    long_description = longDesc,
+    keywords = "JSON RPC JSONRPC WSGI",
+    author = "Lovely Systems GmbH",
+    author_email = "bernddorn@gmail.com",
+    url = 'http://code.google.com/p/lovely-jsonrpc',
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
+    zip_safe = True,
+    include_package_data = False,
+    platforms = ['Any'],
+    namespace_packages = ['lovely'],
+    install_requires = ['setuptools'],
+    extras_require = dict(test=['zope.testing',
+                                'simplejson',
+                                'tornado',
+                                'webtest']),
+    )
